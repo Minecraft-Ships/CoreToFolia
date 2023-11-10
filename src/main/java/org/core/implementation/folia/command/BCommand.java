a89274ee353354e25bc2cad4afcda16d695b654a
mose <nekronmoseley@gmail.com>
2023-11-10 22:49:36 +0000

    Fixed crash reporting

--- src/main/java/org/core/implementation/folia/command/BCommand.java
+++ src/main/java/org/core/implementation/folia/command/BCommand.java
@@ -1,5 +1,6 @@
 package org.core.implementation.folia.command;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -25,20 +26,21 @@ public class BCommand implements TabExecutor {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s,
-            String[] strings) {
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, String[] strings) {
         CommandSource source = ((BukkitPlatform) TranslateCore.getPlatform()).getSource(commandSender);
         try {
             return this.command.run(source, strings);
         } catch (NotEnoughArguments notEnoughArguments) {
-            notEnoughArguments.printStackTrace();
+            commandSender.sendMessage(Component.text(notEnoughArguments.getMessage()));
+        } catch (Throwable e) {
+            e.printStackTrace();
+            commandSender.sendMessage(Component.text(e.getMessage()));
         }
         return false;
     }
 
     @Override
-    public @NotNull List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command,
-            @NotNull String s, String[] strings) {
+    public @NotNull List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, String[] strings) {
         CommandSource source = ((BukkitPlatform) TranslateCore.getPlatform()).getSource(commandSender);
         return this.command.tab(source, strings);
     }
